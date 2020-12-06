8db53e253fc1261e7c38993ba07b71fa92d87bac
Fabricio Olivetti <fabricio.olivetti@gmail.com>
2020-12-06 19:16:00 -0300

    folds and PA

--- day05/src/Main.hs
+++ day05/src/Main.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE ExistentialQuantification #-}
+{-# LANGUAGE BangPatterns #-}
 module Main where
 
 import Data.Char      (digitToInt)
@@ -6,6 +8,39 @@ import Data.List      (sort)
 import Data.Maybe     (catMaybes)
 
 import Data.IntSet    (fromList, IntSet(), member)
+
+
+import Data.Monoid
+import Data.Semigroup
+import Prelude hiding (sum, max, min)
+import Data.Foldable (foldl')
+
+-- from https://github.com/Gabriel439/slides/blob/master/munihac/foldmap.md
+data Fold i o = forall m . Monoid m => Fold (i -> m) (m -> o)
+
+fold' :: Fold i o -> [i] -> o
+fold' (Fold toMonoid summarize) is = summarize folded
+  where
+    folded = foldl' (<>) mempty $ map toMonoid is
+
+data Pair a b = P !a !b 
+              deriving Show 
+
+instance (Semigroup a, Semigroup b) => Semigroup (Pair a b) where
+  (<>) (P aL bL) (P aR bR) = P (aL <> aR) (bL <> bR)
+
+instance (Monoid a, Monoid b) => Monoid (Pair a b) where
+    mempty = P mempty mempty
+
+instance Functor (Fold i) where
+  fmap f (Fold g h) = Fold g (f.h)
+
+(<+>) :: Fold i a -> Fold i b -> Fold i (Pair a b)
+(<+>) (Fold mL sL) (Fold mR sR) = Fold toMonoid summarize
+  where
+    toMonoid x        = P (mL x) (mR x)
+    summarize (P x y) = P (sL x) (sR y)
+
 
 getId :: String -> Int
 getId = fst . head . readInt 2 (`elem` "01") digitToInt . map parser
@@ -36,7 +71,19 @@ withSet (x:xs) ref
   where
     isMissing = (x+2) `member` ref
               && not ( (x+1) `member` ref)
-    
+
+sum, max, min :: (Ord n, Num n, Bounded n) => Fold n n
+sum = Fold Sum getSum
+max = Fold Max getMax
+min = Fold Min getMin
+
+-- | stolen from https://github.com/francesquini/AoC2020/blob/main/src/Problem05.hs
+pa :: Fold Int Int
+pa = diff <$> (sum <+> (sumPA <$> (max <+> min)))
+  where
+    diff (P sumvals total) = total - sumvals
+    sumPA (P mx mn)        = (mx + mn) * (mx - mn + 1) `div` 2
+
 main :: IO ()
 main = do
   seats <- map getId . lines <$> readFile "day05.txt"
@@ -45,3 +92,4 @@ main = do
   print $ findId seats
   print $ findId' $ sort seats
   print $ withSet seats setSeats
+  print $ fold' pa seats
