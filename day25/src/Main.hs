919957886a6250fcecb2c0651f5c204b8e111d54
Fabricio Olivetti <fabricio.olivetti@gmail.com>
2020-12-25 13:05:40 -0300

    integer logarithm

--- day25/src/Main.hs
+++ day25/src/Main.hs
@@ -1,6 +1,27 @@
+{-# language DataKinds #-}
 module Main where
 
-main :: IO ()
-main = do 
+import System.Environment
+import Data.Maybe
+import Math.NumberTheory.Moduli hiding (powMod)
+import Math.NumberTheory.Moduli.Singleton
+import Math.NumberTheory.Powers (powMod)
+
+bruteForce = 
   let crack x = fst $ until ((==x).snd) (\(i,n) -> (i+1, n*7 `rem` 20201227)) (0,1)
-  print $ iterate (\val -> val*8335663 `rem` 20201227) 1 !! crack 8614349
+  in  iterate (\val -> val*8335663 `rem` 20201227) 1 !! crack 8614349
+
+fast = 
+  let cg = fromJust cyclicGroup :: CyclicGroup Integer 20201227
+      rt = fromJust (isPrimitiveRoot cg 7)
+      x  = fromJust (isMultElement 8614349)
+      loop = discreteLogarithm cg rt x
+  in  powMod 8335663 loop 20201227
+
+main :: IO ()
+main = do
+  args <- getArgs
+  case args of
+       []        -> error "Usage: stack run {brute|fast}"
+       ["fast"]  -> print fast
+       ["brute"] -> print bruteForce
